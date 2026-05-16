dbd1bacd1e1b01f48b17f8f65d8632809a9570eb
peacegankam <peacegankam@gmail.com>
2026-05-16 17:16:43 +0100

    Modification de l'affichage dans les tableaux

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -176,7 +176,7 @@ function chargerKPIs() {
       set('kpi-revenus',  formaterMontant(data.revenus_jour || 0));
       set('kpi-revenus-mois', formaterMontant(data.revenus_mensuel || 0));
       set('kpi-revenus-total', formaterMontant(data.revenus_total || 0));
-      set('kpi-ventes',   formaterQuantite(data.ventes_jour || 0) + ' articles');
+      set('kpi-ventes',   formaterQuantite(data.ventes_jour || 0) + ' produits');
       set('kpi-benefice', formaterMontant(data.benefice_jour || 0));
       set('kpi-alertes',  formaterQuantite(data.alertes_stock || 0) + ' produits');
     })
